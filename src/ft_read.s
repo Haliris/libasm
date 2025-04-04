958274018576b1bb976687e6f2b8733dfd19c851
Jean Teissier <jteissie@made-f0Br3s4.clusters.42paris.fr>
2025-04-04 19:51:23 +0200

    Added errno_location to ft_read and ft_write, need to test for correct errno value

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,12 +1,17 @@
 global ft_read
+extern __errno_location
 
 ft_read:
-    mov rax, 0
+    mov     rax, 0
     syscall
-    test rax, rax
-    js error
+    test    rax, rax
+    js      error
     ret
 
 error:
-    mov rax, -1
+    neg     rax
+    mov     rdi, rax
+    call    __errno_location wrt ..plt
+    mov     [rax], rdi
+    mov     rax, -1
     ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,12 +1,17 @@
 global ft_write
+extern __errno_location
 
 ft_write:
-    mov rax, 1
+    mov     rax, 1
     syscall
-    test rax, rax
-    js error
+    test    rax, rax
+    js      error
     ret
 
 error:
-    mov rax, -1
+    neg     rax
+    mov     rdi, rax
+    call    __errno_location wrt ..plt
+    mov     [rax], rdi
+    mov     rax, -1
     ret
