fbb74687131123909160fb50d4937d1e5a0edc52
Jean Teissier <jteissie@made-f0Ar6s3.clusters.42paris.fr>
2025-04-10 22:07:46 +0200

    Restructured repo to abide by _bonus file rules, separated tests to this effect

--- Makefile
+++ Makefile
@@ -14,21 +14,29 @@ ASM_SRC = 	src/ft_strlen.s \
 			src/ft_write.s \
 			src/ft_read.s \
 			src/ft_strdup.s \
-			src/ft_atoi_base.s \
-			src/ft_list_size.s \
-			src/ft_list_push_front.s \
-			src/ft_list_sort.s \
-			src/ft_list_remove_if.s
+			src/ft_atoi_base.s
 
 ASM_OBJ = $(ASM_SRC:.s=.o)
 
+ASM_SRC_BONUS =	src/ft_list_size_bonus.s \
+				src/ft_list_push_front_bonus.s \
+				src/ft_list_sort_bonus.s \
+				src/ft_list_remove_if_bonus.s
+
+
+ASM_OBJ_BONUS = $(ASM_SRC_BONUS:.s=.o)
+
 CC = gcc
 C_FLAGS = -Wall -Wextra -Werror -g3
+
 TEST_SRC = src/main.c
 TEST_INCLUDE = -I src/
 TEST_NAME = unit_tests
 
-all: $(NAME) $(test_name) unit_tests
+TEST_SRC_BONUS = src/main_bonus.c
+TEST_NAME_BONUS = unit_tests_bonus
+
+all: $(NAME) unit_tests
 
 $(NAME): $(ASM_OBJ)
 	$(AR) rcs $@ $^
@@ -39,6 +47,12 @@ $(NAME): $(ASM_OBJ)
 
 unit_tests:
 	$(CC) $(C_FLAGS) $(TEST_SRC) $(NAME) $(TEST_INCLUDE) -o $(TEST_NAME)
+
+bonus: $(ASM_OBJ_BONUS)
+	ar rc $(NAME) $(ASM_OBJ_BONUS)
+
+unit_tests_bonus:
+	$(CC) $(C_FLAGS) $(TEST_SRC_BONUS) $(NAME) $(TEST_INCLUDE) -o $(TEST_NAME_BONUS)
 
 clean:
 	rm -rf $(ASM_OBJ)
--- src/main.c
+++ src/main.c
@@ -1,22 +1,5 @@
 #include "libasm.h"
 
-//set $node = (t_list *)$rdi
-//print *$node
-//
-//print *((t_list *)$rdi)
-//
-//set $n = (t_list *)$rdi
-//while ($n != 0)
-//  print $n->data
-//  set $n = $n->next
-//end
-
-
-typedef struct list {
-    void        *data;
-    struct list *next;
-} t_list;
-
 extern size_t ft_strlen(const char* str);
 extern char   *ft_strcpy(char* dest, const char* src);
 extern int    ft_strcmp(const char* s1, const char* s2);
@@ -24,10 +7,6 @@ extern int    ft_write(int fildes, const void* buff, size_t nbytes);
 extern int    ft_read(int fildes, const void* buff, size_t nbytes);
 extern char   *ft_strdup(const char* s);
 extern int    ft_atoi_base(char *str, char *base);
-extern int    ft_list_size(t_list *list);
-extern void   ft_list_push_front(t_list **head, void *data);
-extern void   ft_list_sort(t_list **begin_list, int (*cmp)());
-extern void   ft_list_remove_if(t_list **begin, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
 
 void run_strcpy_test(const char *test_name, const char *src) {
     char dest[1000]; 
@@ -165,165 +144,7 @@ void test_atoi_base(char *input, char *base, int expected) {
     }
 }
 
-void test_list_size(t_list *head, size_t expected) {
-    size_t result = ft_list_size(head);
-    if (result == expected)
-        printf("PASS: expected %zu, got %zu\n", expected, result);
-    else
-        printf("FAIL: expected %zu, got %zu\n", expected, result);
-}
 
-void print_list_char(t_list *head) {
-    printf("List: ");
-    while (head) {
-        printf("%s -> ", (char *)head->data);
-        head = head->next;
-    }
-    printf("NULL\n");
-}
-
-void print_list_int(t_list *head) {
-    printf("List: ");
-    while (head) {
-        printf("%d -> ", *(int *)head->data);
-        head = head->next;
-    }
-    printf("NULL\n");
-}
-
-int cmp_int(void *a, void *b) {
-//    printf("Comparing %d and %d\n", int_a, int_b); // Debug print
-    if (*(int *)a - *(int *)b > 0)
-        return 0;
-    else
-        return 1;
-}
-
-int is_sorted(t_list *list, int (*cmp)()) {
-    while (list && list->next) {
-        if (cmp(list->data, list->next->data) == 0)
-            return 0;
-        list = list->next;
-    }
-    return 1;
-}
-
-t_list *create_list_from_array(int *arr, int size) {
-    t_list *head = NULL;
-    for (int i = size - 1; i >= 0; --i) {
-        t_list *new_node = malloc(sizeof(t_list));
-        new_node->data = &arr[i];
-        new_node->next = head;
-        head = new_node;
-    }
-    return head;
-}
-
-void free_list(t_list *head) {
-    while (head) {
-        t_list *tmp = head;
-        head = head->next;
-        free(tmp);
-    }
-}
-
-void test_list_push_front(void) {
-    t_list *head = NULL;
-
-    ft_list_push_front(&head, "World");
-    print_list_char(head);
-    ft_list_push_front(&head, "Hello");
-    print_list_char(head);
-
-    if (head && strcmp((char *)head->data, "Hello") == 0)
-        printf("PASS: Head is Hello\n");
-    else
-        printf("FAIL: Head is not Hello\n");
-
-    // Push another
-    ft_list_push_front(&head, "Start");
-    print_list_char(head);
-
-    // Edge case: pushing to an existing list
-    ft_list_push_front(&head, "");
-    print_list_char(head);
-
-}
-
-void test_list_sort(int *array, int size) {
-    printf("Test: sorting list of %d elements\n", size);
-
-    t_list *list = create_list_from_array(array, size);
-    printf("Before: ");
-    print_list_int(list);
-
-    ft_list_sort(&list, cmp_int);
-
-    printf("After:  ");
-    print_list_int(list);
-
-    if(is_sorted(list, cmp_int)) {
-        printf("✅ Passed!\n\n");
-    }
-    else {
-        printf("Failed!\n\n");
-    }
-    free_list(list);
-}
-
-void dummy_free(void *data) {
-    (void)data;
-    // nothing needed here, since data points to stack ints
-    return;
-}
-
-int is_equal_int(void *a, void *b) {
-    return (*(int *)a == *(int *)b);
-}
-
-void test_list_remove_if(void) {
-    printf("\n----/list_remove_if tests/----\n");
-
-    int a1[] = {1, 2, 3, 4, 5};
-    t_list *list1 = create_list_from_array(a1, 5);
-    int ref1 = 3;
-    ft_list_remove_if(&list1, &ref1, is_equal_int, dummy_free);
-    printf("Remove 3 from [1 2 3 4 5]: ");
-    print_list_int(list1);
-    free_list(list1);
-
-    int a2[] = {1, 1, 1, 1};
-    t_list *list2 = create_list_from_array(a2, 4);
-    int ref2 = 1;
-    ft_list_remove_if(&list2, &ref2, is_equal_int, dummy_free);
-    printf("Remove all 1s from [1 1 1 1]: ");
-    print_list_int(list2);
-    free_list(list2);
-
-    int a3[] = {1, 2, 3, 4};
-    t_list *list3 = create_list_from_array(a3, 4);
-    int ref3 = 99;
-    ft_list_remove_if(&list3, &ref3, is_equal_int, dummy_free);
-    printf("Remove 99 (not present) from [1 2 3 4]: ");
-    print_list_int(list3);
-    free_list(list3);
-
-    int a4[] = {};
-    t_list *list4 = create_list_from_array(a4, 0);
-    int ref4 = 1;
-    ft_list_remove_if(&list4, &ref4, is_equal_int, dummy_free);
-    printf("Remove 1 from empty list: ");
-    print_list_int(list4);
-    free_list(list4);
-
-    int a5[] = {2};
-    t_list *list5 = create_list_from_array(a5, 1);
-    int ref5 = 2;
-    ft_list_remove_if(&list5, &ref5, is_equal_int, dummy_free);
-    printf("Remove 2 from single-element [2]: ");
-    print_list_int(list5);
-    free_list(list5);
-}
 
 int main(void) {
 
@@ -421,53 +242,6 @@ int main(void) {
     test_atoi_base("000", "0123456789", 0);
     test_atoi_base("   +0", "0123456789", 0);
     test_atoi_base("   -ff", "0123456789abcdef", -255);
-
-    printf("\n----/list_size tests/----\n");
-    test_list_size(NULL, 0);
-
-    t_list node1 = { .data = NULL, .next = NULL };
-    test_list_size(&node1, 1);
-
-    t_list node2 = { .data = NULL, .next = NULL };
-    node1.next = &node2;
-    test_list_size(&node1, 2);
-
-    t_list node3 = { .data = NULL, .next = NULL };
-    node2.next = &node3;
-    test_list_size(&node1, 3);
-
-    t_list *nodes = malloc(sizeof(t_list) * 10);
-    for (int i = 0; i < 9; ++i) {
-        nodes[i].data = NULL;
-        nodes[i].next = &nodes[i + 1];
-    }
-    nodes[9].data = NULL;
-    nodes[9].next = NULL;
-    test_list_size(&nodes[0], 10);
-
-    printf("\n----/list_push_front tests/----\n");
-    test_list_push_front();
-    
-    printf("\n----/list_sort tests/----\n");
-    int a[] = {5, 3, 8, 1, 2};
-    test_list_sort(a, 5);
-
-    int b[] = {1, 2, 3, 4, 5}; // already sorted
-    test_list_sort(b, 5);
-
-    int c[] = {5, 4, 3, 2, 1}; // reverse order
-    test_list_sort(c, 5);
-
-    int d[] = {42}; // single element
-    test_list_sort(d, 1);
-
-    int e[] = {}; // empty list
-    test_list_sort(e, 0);
-
-    int f[] = {7, 7, 7, 7}; // duplicates
-    test_list_sort(f, 4);
-
-    test_list_remove_if();
 
     return 0;
 }
